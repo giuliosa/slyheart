57891d99046d06bac3d18308459d2dcc0a8f73ca
Giulio Sá <giuliocaetano@yahoo.com.br>
2024-07-13 18:00:50 -0300

    plus ultra

--- Scenes/Actors/Player/plataform_player.gd
+++ Scenes/Actors/Player/plataform_player.gd
@@ -5,6 +5,7 @@ const SPEED = 550.0
 const AIR_FRICTION = 0.5
 
 var is_jumping: bool
+var can_jump := true
 var life := 10
 var knockback_vector := Vector2.ZERO
 var knockback_power := 20
@@ -22,6 +23,8 @@ var fall_gravity
 
 @onready var player_body := $Body as Sprite2D
 @onready var remote_transform := $Remote as RemoteTransform2D
+@onready var life_label := $Life as Label
+@onready var coyote_timer := $CoyoteTimer as Timer
 
 func _ready():
 	jump_velocity = (jump_height * 2) / max_time_to_peak
@@ -29,38 +32,41 @@ func _ready():
 	fall_gravity = gravity * 2
 
 func _physics_process(delta):	
+	life_label.text = str(life)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		#velocity.y += gravity * delta
 		velocity.x = 0
 
-	if is_on_wall():
-		#velocity.y = 200
+	if is_on_wall_only() and not is_on_floor():
+		#velocity.y = fall_gravity
 		wall_normal = get_wall_normal()
 
 	# Handle jump.
-	if Input.is_action_just_pressed("action") and jump_count < max_jumps:
-		print("Pulou do chão")
+	if Input.is_action_just_pressed("action") and jump_count < max_jumps and can_jump:
 		velocity.y = -jump_velocity
 		is_jumping = true
-		print(velocity)
 		jump_count += 1
 			
 			
 		if is_on_wall():
 			jump_count = 1
-			print("pulou da parede")
+			print(wall_normal)
 			if wall_normal == Vector2.LEFT:
-				velocity = Vector2(wall_normal.x * jump_velocity, -jump_velocity)
+				velocity = lerp(wall_normal, Vector2(wall_normal.x * jump_velocity, -jump_velocity), 1.8)
 			if wall_normal == Vector2.RIGHT:
-				velocity = Vector2(wall_normal.x * jump_velocity, -jump_velocity)
-			
-			print(velocity)
+				velocity = lerp(wall_normal, Vector2(wall_normal.x * jump_velocity, -jump_velocity), 1.8)
 			
 			
 	elif is_on_floor():
 		is_jumping = false
 		jump_count = 0
+		
+	if is_on_floor() and !can_jump:
+		can_jump = true
+	elif can_jump and coyote_timer.is_stopped():
+		coyote_timer.start()
 	
 	if velocity.y > 0 or not Input.is_action_pressed("action"):
 		velocity.y += fall_gravity * delta
@@ -88,9 +94,8 @@ func follow_camera(camera):
 	remote_transform.remote_path = camera_path
 
 func _on_hurtbox_body_entered(body):
-	print(life)
 	var knockback = Vector2((global_position.x - body.global_position.x) * knockback_power, -200)
-	if life < 0:
+	if life <= 0:
 		queue_free()
 	else:		
 		take_damage(knockback)
@@ -105,3 +110,7 @@ func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
 		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
 		player_body.modulate = Color(1, 0, 0, 1)
 		knockback_tween.parallel().tween_property(player_body, "modulate", Color(1,1,1,1), duration)
+
+
+func _on_coyote_timer_timeout():
+	can_jump = false
