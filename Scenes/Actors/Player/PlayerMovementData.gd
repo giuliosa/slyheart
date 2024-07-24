701bd6a6304ae30880a23508cb03878df29e894d
Giulio Sá <giuliocaetano@yahoo.com.br>
2024-07-24 09:03:45 -0300

    Double jump and wall jump

---
+++ Scenes/Actors/Player/PlayerMovementData.gd
@@ -0,0 +1,10 @@
+class_name PlayerMovementData
+extends Resource
+
+@export var speed = 300.0
+@export var acceleration = 600.0
+@export var friction = 1400.0
+@export var jump_velocity = -500.0
+@export var gravity_scale = 1.0
+@export var air_resistence = 200.0
+@export var air_acceleration = 300.0
--- Scenes/Actors/Player/player.gd
+++ Scenes/Actors/Player/player.gd
@@ -1,11 +1,8 @@
 extends CharacterBody2D
 
+@export var movement_data: PlayerMovementData
 
-const SPEED = 300.0
-const ACCELERATION = 600.0
-const FRICTION = 1400.0
-const JUMP_VELOCITY = -500.0
-
+var air_jump = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var body = $Body
@@ -13,9 +10,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	apply_gravity(delta)
+	handle_wall_jump()	
 	handle_jump() 
-	handle_direction(Input.get_axis("ui_left", "ui_right"), delta)
-	var was_on_floor = is_on_floor()	
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	handle_air_acceleration(input_axis, delta)
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	apply_air_resistance(input_axis, delta)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge:
@@ -23,20 +25,48 @@ func _physics_process(delta):
 
 func apply_gravity(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * movement_data.gravity_scale * delta
 
 func handle_jump():
+	if is_on_floor(): air_jump = true
+	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("action"):
-			velocity.y = JUMP_VELOCITY
+			velocity.y = movement_data.jump_velocity
 	if not is_on_floor():
-		if Input.is_action_just_pressed("action") and velocity.y < JUMP_VELOCITY / 2:
-			velocity.y = JUMP_VELOCITY / 2
+		if Input.is_action_just_pressed("action") and velocity.y < movement_data.jump_velocity / 2:
+			velocity.y = movement_data.jump_velocity / 2
+			
+		if Input.is_action_just_pressed("action") and air_jump:
+			velocity.y = movement_data.jump_velocity * 0.8
+			air_jump = false
 
-func handle_direction(direction, delta):
-	if direction:
-		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
+func handle_wall_jump():
+	if not is_on_wall_only(): return
+	if Input.is_action_just_pressed("action") and get_wall_normal():
+		velocity.x = get_wall_normal().x * movement_data.speed
+		velocity.y = movement_data.jump_velocity 
+			
+func handle_acceleration(input_axis, delta):
+	if not is_on_floor(): return
+	if input_axis:
+		velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.acceleration * delta)
 		#Change this when the character have the properly sprite
-		body.flip_h = direction > 0
-	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		body.flip_h = input_axis > 0
+
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor(): return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_acceleration * delta)
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
+		
+
+func apply_air_resistance(input_axis, delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistence * delta)
+
+
+	
